a4895666c5bf1a2e0c64ce6aac36a3d83bb51633
Shujia Huang <huangshujia9@gmail.com>
2021-07-16 14:59:23 +0800

    commit myfirst.cpp

--- source/chapter02/myfirst.cpp
+++ source/chapter02/myfirst.cpp
@@ -1,15 +1,20 @@
-// myfirst.cpp--displays a message
+/* displays a message
+ */
 
 #include <iostream>                           // a PREPROCESSOR directive
-int main()                                    // function header
-{                                             // start of function body
+
+int main() {                                 // function header
+    // start of function body
     using namespace std;                      // make definitions visible
-    cout << "Come up and C++ me some time.";  // message
-    cout << endl;                             // start a new line
-    cout << "You won't regret it!" << endl;   // more output
-// If the output window closes before you can read it,
-// add the following code:
-    // cout << "Press any key to continue." <<endl;
-	// cin.get();                                                   
+    cout << "Hello world!" << endl;           // message
+
+    cout << "Comme up and C++ me some time."
+         << endl;
+    cout << "You won't regret it! " << endl;
+
+    // If the output window closes before you can read it,
+    // add the following code:
+    cout << "Press any key to continue." << endl;
+    cin.get();
     return 0;                                 // terminate main()
 }                                             // end of function body
